03f15b2e6feae2b3ebe14c24fe436951a1884305
Amirreza Azadmehr <amirreza.azadmehr33@hotmail.com>
2022-11-14 13:24:35 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
-# Amirreza_Azadmehr
-# Amirreza.azadmehr33@hotmail.com
 from tronpy import Tron
+
 client = Tron()
+
 print('wallet :' , client.generate_address()['base58check_address'])
 print('Private key :',client.generate_address()['private_key'])
